839bba613adb228af59d6a6ba7aa7d9a27f6f3fa
Muhammadrizo15 <iozxckali@gmail.com>
2023-01-02 21:14:57 +0500

    fa

--- src/Components/Explore/Explore.jsx
+++ src/Components/Explore/Explore.jsx
@@ -26,10 +26,14 @@ function App() {
             <div className="explore__content wrap__content">
 
                 <div className="explore__wrapper__left">
-                    {post.map(posts => (
-                        <div className='gellery__item' key={posts.url}>
+
+                </div>
+
+                <div className="explore__wrapper__left">
+                    <div className="gellery__item__wrap">
+                        <div className='gellery__item' onClick={() => { setShow2(true) }}>
                             {/* Photo */}
-                            <img src={posts.url} alt="" />
+                            <img src="../images/explore/7/5.jpg" alt="" />
                             {/* Photo likes and coments */}
                             <div className="gallery-item-info">
                                 <ul>
@@ -50,7 +54,103 @@ function App() {
                                 </span>
                             </div>
                         </div>
-                    ))}
+                        {show2 && <Popup props='../images/explore/7/5.jpg' />}
+                    </div>
+                    <div className="gellery__item__wrap">
+                        <div className='gellery__item' onClick={() => { setShow(true) }}>
+                            {/* Photo */}
+                            <img src="../images/explore/2/1.jpg" alt="" />
+                            {/* Photo likes and coments */}
+                            <div className="gallery-item-info">
+                                <ul>
+                                    <li className="gallery-item-likes">
+                                        <img src="../images/explore/heart.png" alt="" />
+                                        56
+                                    </li>
+                                    <li className="gallery-item-comments">
+                                        <img src="../images/explore/chat.png" alt="" />
+                                        2
+                                    </li>
+                                </ul>
+                            </div>
+                            {/* What is this photo video or photo */}
+                            <div className="gallery-item-type">
+                                <span className="visually-hidden">
+                                    <img src="../images/explore/photo.png" alt="" />
+                                </span>
+                            </div>
+                        </div>
+                        {show && <Popup props='../images/explore/2/1.jpg' />}
+                    </div>
+                    <div className='gellery__item'>
+                        {/* Photo */}
+                        <img src="../images/explore/1/1.png" alt="" />
+                        {/* Photo likes and coments */}
+                        <div className="gallery-item-info">
+                            <ul>
+                                <li className="gallery-item-likes">
+                                    <img src="../images/explore/heart.png" alt="" />
+                                    56
+                                </li>
+                                <li className="gallery-item-comments">
+                                    <img src="../images/explore/chat.png" alt="" />
+                                    2
+                                </li>
+                            </ul>
+                        </div>
+                        {/* What is this photo video or photo */}
+                        <div className="gallery-item-type">
+                            <span className="visually-hidden">
+                                <img src="../images/explore/photo.png" alt="" />
+                            </span>
+                        </div>
+                    </div>
+                    <div className='gellery__item'>
+                        {/* Photo */}
+                        <img src="../images/explore/1/2.png" alt="" />
+                        {/* Photo likes and coments */}
+                        <div className="gallery-item-info">
+                            <ul>
+                                <li className="gallery-item-likes">
+                                    <img src="../images/explore/heart.png" alt="" />
+                                    56
+                                </li>
+                                <li className="gallery-item-comments">
+                                    <img src="../images/explore/chat.png" alt="" />
+                                    2
+                                </li>
+                            </ul>
+                        </div>
+                        {/* What is this photo video or photo */}
+                        <div className="gallery-item-type">
+                            <span className="visually-hidden">
+                                <img src="../images/explore/photo.png" alt="" />
+                            </span>
+                        </div>
+                    </div>
+                    <div className='gellery__item ffafd'>
+                        {/* Photo */}
+                        <img src="../images/explore/5/1.png" alt="" />
+                        {/* Photo likes and coments */}
+                        <div className="gallery-item-info">
+                            <ul>
+                                <li className="gallery-item-likes">
+                                    <img src="../images/explore/heart.png" alt="" />
+                                    56
+                                </li>
+                                <li className="gallery-item-comments">
+                                    <img src="../images/explore/chat.png" alt="" />
+                                    2
+                                </li>
+                            </ul>
+                        </div>
+                        {/* What is this photo video or photo */}
+                        <div className="gallery-item-type">
+                            <span className="visually-hidden">
+                                <img src="../images/explore/photo.png" alt="" />
+                            </span>
+                        </div>
+                    </div>
 
 
 
@@ -59,12 +159,11 @@ function App() {
 
 
                 </div>
-
                 <div className="explore__wrapper__left">
-                    {post.map(posts => (
-                        <div className='gellery__item' key={posts.url}>
+                    <div className="gellery__item__wrap">
+                        <div className='gellery__item' onClick={() => { setShow2(true) }}>
                             {/* Photo */}
-                            <img src={posts.url} alt="" />
+                            <img src="../images/explore/7/5.jpg" alt="" />
                             {/* Photo likes and coments */}
                             <div className="gallery-item-info">
                                 <ul>
@@ -85,7 +184,103 @@ function App() {
                                 </span>
                             </div>
                         </div>
-                    ))}
+                        {show2 && <Popup props='../images/explore/7/5.jpg' />}
+                    </div>
+                    <div className="gellery__item__wrap">
+                        <div className='gellery__item' onClick={() => { setShow(true) }}>
+                            {/* Photo */}
+                            <img src="../images/explore/2/1.jpg" alt="" />
+                            {/* Photo likes and coments */}
+                            <div className="gallery-item-info">
+                                <ul>
+                                    <li className="gallery-item-likes">
+                                        <img src="../images/explore/heart.png" alt="" />
+                                        56
+                                    </li>
+                                    <li className="gallery-item-comments">
+                                        <img src="../images/explore/chat.png" alt="" />
+                                        2
+                                    </li>
+                                </ul>
+                            </div>
+                            {/* What is this photo video or photo */}
+                            <div className="gallery-item-type">
+                                <span className="visually-hidden">
+                                    <img src="../images/explore/photo.png" alt="" />
+                                </span>
+                            </div>
+                        </div>
+                        {show && <Popup props='../images/explore/2/1.jpg' />}
+                    </div>
+                    <div className='gellery__item'>
+                        {/* Photo */}
+                        <img src="../images/explore/1/1.png" alt="" />
+                        {/* Photo likes and coments */}
+                        <div className="gallery-item-info">
+                            <ul>
+                                <li className="gallery-item-likes">
+                                    <img src="../images/explore/heart.png" alt="" />
+                                    56
+                                </li>
+                                <li className="gallery-item-comments">
+                                    <img src="../images/explore/chat.png" alt="" />
+                                    2
+                                </li>
+                            </ul>
+                        </div>
+                        {/* What is this photo video or photo */}
+                        <div className="gallery-item-type">
+                            <span className="visually-hidden">
+                                <img src="../images/explore/photo.png" alt="" />
+                            </span>
+                        </div>
+                    </div>
+                    <div className='gellery__item'>
+                        {/* Photo */}
+                        <img src="../images/explore/1/2.png" alt="" />
+                        {/* Photo likes and coments */}
+                        <div className="gallery-item-info">
+                            <ul>
+                                <li className="gallery-item-likes">
+                                    <img src="../images/explore/heart.png" alt="" />
+                                    56
+                                </li>
+                                <li className="gallery-item-comments">
+                                    <img src="../images/explore/chat.png" alt="" />
+                                    2
+                                </li>
+                            </ul>
+                        </div>
+                        {/* What is this photo video or photo */}
+                        <div className="gallery-item-type">
+                            <span className="visually-hidden">
+                                <img src="../images/explore/photo.png" alt="" />
+                            </span>
+                        </div>
+                    </div>
+                    <div className='gellery__item ffafd'>
+                        {/* Photo */}
+                        <img src="../images/explore/5/1.png" alt="" />
+                        {/* Photo likes and coments */}
+                        <div className="gallery-item-info">
+                            <ul>
+                                <li className="gallery-item-likes">
+                                    <img src="../images/explore/heart.png" alt="" />
+                                    56
+                                </li>
+                                <li className="gallery-item-comments">
+                                    <img src="../images/explore/chat.png" alt="" />
+                                    2
+                                </li>
+                            </ul>
+                        </div>
+                        {/* What is this photo video or photo */}
+                        <div className="gallery-item-type">
+                            <span className="visually-hidden">
+                                <img src="../images/explore/photo.png" alt="" />
+                            </span>
+                        </div>
+                    </div>
 
 
 
